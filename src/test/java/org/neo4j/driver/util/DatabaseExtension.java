5838e1899887dbe580fbae89c8c8d5deeca2b8f5
excel <admin@excel>
2024-04-26 10:47:22 +0800

    use local container

--- src/test/java/org/neo4j/driver/util/DatabaseExtension.java
+++ src/test/java/org/neo4j/driver/util/DatabaseExtension.java
@@ -18,116 +18,49 @@
  */
 package org.neo4j.driver.util;
 
-import static org.neo4j.driver.util.Neo4jSettings.BOLT_TLS_LEVEL;
-import static org.neo4j.driver.util.Neo4jSettings.BoltTlsLevel.OPTIONAL;
-import static org.neo4j.driver.util.Neo4jSettings.SSL_POLICY_BOLT_CLIENT_AUTH;
-import static org.neo4j.driver.util.Neo4jSettings.SSL_POLICY_BOLT_ENABLED;
+import org.junit.jupiter.api.extension.*;
+import org.neo4j.driver.*;
+import org.neo4j.driver.internal.BoltServerAddress;
+import org.neo4j.driver.types.TypeSystem;
+import org.testcontainers.utility.MountableFile;
+import scala.xml.dtd.REQUIRED;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URI;
-import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.github.dockerjava.api.command.InspectContainerResponse;
-import com.github.dockerjava.api.model.ExposedPort;
-import com.github.dockerjava.api.model.Ports;
-import org.bouncycastle.asn1.x509.GeneralName;
-import org.junit.jupiter.api.extension.AfterAllCallback;
-import org.junit.jupiter.api.extension.AfterEachCallback;
-import org.junit.jupiter.api.extension.BeforeEachCallback;
-import org.junit.jupiter.api.extension.ConditionEvaluationResult;
-import org.junit.jupiter.api.extension.ExecutionCondition;
-import org.junit.jupiter.api.extension.ExtensionContext;
-import org.neo4j.driver.AuthToken;
-import org.neo4j.driver.AuthTokens;
-import org.neo4j.driver.Config;
-import org.neo4j.driver.Driver;
-import org.neo4j.driver.GraphDatabase;
-import org.neo4j.driver.internal.BoltServerAddress;
-import org.neo4j.driver.types.TypeSystem;
-import org.neo4j.driver.util.CertificateUtil.CertificateKeyPair;
-import org.testcontainers.DockerClientFactory;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.Network;
-import org.testcontainers.images.builder.ImageFromDockerfile;
-import org.testcontainers.shaded.com.google.common.base.Preconditions;
-import org.testcontainers.utility.MountableFile;
-import scala.xml.dtd.REQUIRED;
+import static org.neo4j.driver.util.Neo4jSettings.*;
+import static org.neo4j.driver.util.Neo4jSettings.BoltTlsLevel.OPTIONAL;
 
 public class DatabaseExtension implements ExecutionCondition, BeforeEachCallback, AfterEachCallback, AfterAllCallback {
-    private static final int PANDA_PORT = 7600;
 
-    private static final boolean dockerAvailable;
     private static final DatabaseExtension instance;
-    private static final URI pandaUri;
+    private static final URI pandaUri = URI.create("panda://localhost:7601");
     private static final AuthToken authToken;
-    private static final File cert;
-    private static final File key;
-    private static final Network network;
+
     private static final Map<String, String> defaultConfig;
 
-    private static GenericContainer<?> pandaContainer;
     private static Driver driver;
 
     static {
-        dockerAvailable = isDockerAvailable();
         instance = new DatabaseExtension();
         defaultConfig = new HashMap<>();
         defaultConfig.put(SSL_POLICY_BOLT_ENABLED, "true");
         defaultConfig.put(SSL_POLICY_BOLT_CLIENT_AUTH, "NONE");
         defaultConfig.put(BOLT_TLS_LEVEL, OPTIONAL.toString());
 
-        if (dockerAvailable) {
-            CertificateKeyPair<File, File> pair = generateCertificateAndKey();
-            cert = pair.cert();
-            key = pair.key();
-
-            network = Network.newNetwork();
-            pandaContainer = setupPandaContainer(cert, key, defaultConfig);
-            pandaContainer.start();
-            pandaUri = URI.create(String.format("panda://" + getHost() + ":" + getMappedPort(PANDA_PORT)));
-            authToken = AuthTokens.basic("panda", "panda");
-            driver = GraphDatabase.driver(pandaUri, authToken);
-            waitForBoltAvailability();
-        } else {
-            // stub init, this is not usable when Docker is unavailable
-            pandaUri = URI.create("");
-            authToken = AuthTokens.none();
-            cert = new File("");
-            key = new File("");
-            network = null;
-        }
-    }
-
-    private static String getHost(){
-       return DockerClientFactory.instance().dockerHostIpAddress();
-    }
-
-    private static int getMappedPort(int originalPort){
-        Preconditions.checkState(pandaContainer.getContainerId() != null, "Mapped port can only be obtained after the container is started");
-        Ports.Binding[] binding = new Ports.Binding[0];
-        InspectContainerResponse containerInfo = pandaContainer.getContainerInfo();
-        if (containerInfo != null) {
-            binding = (Ports.Binding[])containerInfo.getNetworkSettings().getPorts().getBindings().get(new ExposedPort(originalPort));
-        }
-
-        if (binding != null && binding.length > 0 && binding[0] != null) {
-            return Integer.valueOf(binding[0].getHostPortSpec());
-        } else {
-            throw new IllegalArgumentException("Requested port (" + originalPort + ") is not mapped");
-        }
+        authToken = AuthTokens.basic("panda", "panda");
+        driver = GraphDatabase.driver(pandaUri, authToken);
     }
 
     @Override
     public ConditionEvaluationResult evaluateExecutionCondition(ExtensionContext context) {
-        return dockerAvailable
-                ? ConditionEvaluationResult.enabled("Docker is available")
-                : ConditionEvaluationResult.disabled("Docker is unavailable");
+        return ConditionEvaluationResult.enabled("Server is available");
     }
 
     @Override
@@ -137,40 +70,12 @@ public class DatabaseExtension implements ExecutionCondition, BeforeEachCallback
 
     @Override
     public void afterEach(ExtensionContext context) throws Exception {
+//        System.out.println(pandaContainer.getLogs());
     }
 
     @Override
     public void afterAll(ExtensionContext context) {
 //        deleteAndStartNeo4j(Collections.emptyMap());
-    }
-
-    public Driver driver() {
-        return driver;
-    }
-
-    public TypeSystem typeSystem() {
-        return driver.defaultTypeSystem();
-    }
-
-    public void deleteAndStartNeo4j(Map<String, String> config) {
-        Map<String, String> updatedConfig = new HashMap<>(defaultConfig);
-        updatedConfig.putAll(config);
-
-        pandaContainer.stop();
-        pandaContainer = setupPandaContainer(cert, key, updatedConfig);
-        pandaContainer.start();
-        if (REQUIRED.toString().equals(config.get(BOLT_TLS_LEVEL))) {
-            driver = GraphDatabase.driver(
-                    pandaUri,
-                    authToken,
-                    Config.builder()
-                            .withTrustStrategy(Config.TrustStrategy.trustCustomCertificateSignedBy(cert))
-                            .withEncryption()
-                            .build());
-        } else {
-            driver = GraphDatabase.driver(pandaUri, authToken);
-        }
-        waitForBoltAvailability();
     }
 
     public String addImportFile(String prefix, String suffix, String contents) throws IOException {
@@ -179,11 +84,18 @@ public class DatabaseExtension implements ExecutionCondition, BeforeEachCallback
         try (PrintWriter out = new PrintWriter(tmpFile)) {
             out.println(contents);
         }
-        Path tmpFilePath = tmpFile.toPath();
-        Path targetPath =
-                Paths.get("/var/lib/neo4j/import", tmpFilePath.getFileName().toString());
-        pandaContainer.copyFileToContainer(MountableFile.forHostPath(tmpFilePath), targetPath.toString());
         return String.format("file:///%s", tmpFile.getName());
+    }
+
+    public void deleteAndStartNeo4j(Map<String, String> config) {
+    }
+
+    public Driver driver() {
+        return driver;
+    }
+
+    public TypeSystem typeSystem() {
+        return driver.defaultTypeSystem();
     }
 
     public URI uri() {
@@ -206,50 +118,6 @@ public class DatabaseExtension implements ExecutionCondition, BeforeEachCallback
         return new BoltServerAddress(pandaUri);
     }
 
-    public void updateEncryptionKeyAndCert(File key, File cert) {
-        System.out.println("Updated neo4j key and certificate file.");
-        pandaContainer.stop();
-        pandaContainer = setupPandaContainer(cert, key, defaultConfig);
-        pandaContainer.start();
-        driver = GraphDatabase.driver(pandaUri, authToken);
-        waitForBoltAvailability();
-    }
-
-    public File tlsCertFile() {
-        return cert;
-    }
-
-//    public void startProxy() {
-//        try {
-//            nginx.execInContainer("nginx");
-//        } catch (IOException | InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
-//        nginxRunning = true;
-//    }
-//
-//    public void stopProxy() {
-//        try {
-//            nginx.execInContainer("nginx", "-s", "stop");
-//        } catch (IOException | InterruptedException e) {
-//            throw new RuntimeException(e);
-//        }
-//        nginxRunning = false;
-//    }
-
-//    private boolean isNeo4jVersionOrEarlier(int major, int minor) {
-//        try (Session session = driver.session()) {
-//            String neo4jVersion = session.readTransaction(
-//                    tx -> tx.run("CALL dbms.components() YIELD versions " + "RETURN versions[0] AS version")
-//                            .single()
-//                            .get("version")
-//                            .asString());
-//            String[] versions = neo4jVersion.split("\\.");
-//            return parseInt(versions[0]) <= major && parseInt(versions[1]) <= minor;
-//        }
-//        return false;
-//    }
-
     public boolean isNeo4j44OrEarlier() {
         return true;
     }
@@ -258,80 +126,4 @@ public class DatabaseExtension implements ExecutionCondition, BeforeEachCallback
         return instance;
     }
 
-    public static GeneralName getDockerHostGeneralName() {
-        String host = DockerClientFactory.instance().dockerHostIpAddress();
-        GeneralName generalName;
-        try {
-            generalName = new GeneralName(GeneralName.iPAddress, host);
-        } catch (IllegalArgumentException e) {
-            generalName = new GeneralName(GeneralName.dNSName, host);
-        }
-        return generalName;
-    }
-
-    private static CertificateKeyPair<File, File> generateCertificateAndKey() {
-        try {
-            return CertificateUtil.createNewCertificateAndKey(getDockerHostGeneralName());
-        } catch (Throwable e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static GenericContainer<?> setupPandaContainer(File cert, File key, Map<String, String> config) {
-        var image = new ImageFromDockerfile()
-                .withFileFromPath(".", FileSystems.getDefault().getPath("/home/wanghuajin/pandadb-bin"))
-                .withDockerfileFromBuilder(builder ->
-                        builder
-                                .from("openjdk:11")
-                                .copy("pandadb-server-0.4.7","pandadb-server")
-                                .entryPoint("/pandadb-server/bin/pandadb start && tail -f /pandadb-server/logs/pandadb.log")
-                                .expose(PANDA_PORT)
-                                .build());
-        return new GenericContainer(image).withExposedPorts(PANDA_PORT).withReuse(true);
-    }
-
-//    private static GenericContainer<?> setupNginxContainer() {
-//        ImageFromDockerfile extendedNginxImage = new ImageFromDockerfile()
-//                .withDockerfileFromBuilder(builder -> builder.from("nginx:1.23.0-alpine")
-//                        .copy("nginx.conf", "/etc/nginx/")
-//                        .build())
-//                .withFileFromClasspath("nginx.conf", "nginx.conf");
-//
-//        //noinspection rawtypes
-//        return new GenericContainer(extendedNginxImage.get())
-//                .withNetwork(network)
-//                .withExposedPorts(BOLT_PORT, HTTP_PORT)
-//                .withCommand("sh", "-c", "nginx && while sleep 3600; do :; done");
-//    }
-
-    private static void waitForBoltAvailability() {
-        int maxAttempts = 600;
-        for (int attempt = 0; attempt < maxAttempts; attempt++) {
-            try {
-                driver.verifyConnectivity();
-                return;
-            } catch (RuntimeException verificationException) {
-                if (attempt == maxAttempts - 1) {
-                    throw new RuntimeException(
-                            "Timed out waiting for Neo4j to become available over Bolt", verificationException);
-                }
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException interruptedException) {
-                    interruptedException.addSuppressed(verificationException);
-                    throw new RuntimeException(
-                            "Interrupted while waiting for Neo4j to become available over Bolt", interruptedException);
-                }
-            }
-        }
-    }
-
-    private static boolean isDockerAvailable() {
-        try {
-            DockerClientFactory.instance().client();
-            return true;
-        } catch (Throwable ex) {
-            return false;
-        }
-    }
 }
